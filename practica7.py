12014d12d98070106b2aaf68b5135b7b9fc4cd44
amelia588 <1473967@senati.pe>
2023-03-04 09:33:45 -0500

    use el valor len

--- practica7.py
+++ practica7.py
@@ -3,3 +3,8 @@ lista=["red","blue",...,"white",]
 #primer elemento: red
 #ultimo elemento: while
 
+primer_elemento=lista[0]
+#ultimo elemento
+ultimo_elemento=lista[len(lista)-1]
+print(primer_elemento)
+print(ultimo_elemento)
