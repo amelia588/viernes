f2a23456109c59615bdeb97db72ccd3a65696ae6
amelia588 <1473967@senati.pe>
2023-03-04 09:27:07 -0500

    iniciamos

---
+++ practica7.py
@@ -0,0 +1,5 @@
+#Escribir un codigo de programcio donde me permite imprimir el primer y el ultimo valor de lista.
+lista=["red","blue",...,"white",]
+#primer elemento: red
+#ultimo elemento: while
+
