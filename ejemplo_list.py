b25a4c261240ce992a71d8aa0e7b9e06dc14e8f8
amelia588 <1473967@senati.pe>
2023-03-04 08:25:00 -0500

    subiendo lista

---
+++ ejemplo_list.py
@@ -0,0 +1,7 @@
+# lista: coleccion de elemetos que podemos modificar (mutable)
+# tupla: coleccion de elementos que no modificar(imutable)
+
+asignaturas1_lista=[10,15,27,100,200]
+asignaturas2_tuple=('10','15','27','100','200')
+print(asignaturas1_lista)
+print(asignaturas2_tuple)
---
+++ ejemploslist.py
@@ -0,0 +1,16 @@
+numbers=list['10','15','27','100','200']
+print(numbers)
+print(type(numbers))
+
+numbers=(10,15,27,100,200)
+print(numbers)
+print(type(numbers))
+
+#lista
+print(numbers[10,15,27,100,200])
+print(numbers(10,15,27,100,200))
+
+list=[10,15,27,100,200]
+
+
+
